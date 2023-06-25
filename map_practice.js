749b364b14c050fa96cccea7b78ef720770f1850
sbkim1126 <subink1126@gmail.com>
2023-06-25 18:05:54 +0900

    oh

---
+++ map_practice.js
@@ -0,0 +1,26 @@
+// const nums = Array(20).map((_, i) => i);
+// console.log(nums[0]);
+
+// const arr = Array(5).fill().map((i, s) => s);
+// console.log(arr);
+
+// let arr1 = Array(5).fill().map((_, element) => {
+//     console.log(element);
+//     element*3;
+// });
+// console.log(arr1);
+
+// const arr2 = Array(5).fill().map(function(item, index){
+//     console.log(index+"번 값", item);
+// });
+
+const arr = [10, 20, 30, 40, 50];
+
+arr.map(function(item, index) {
+    console.log(index+"번 값", item);
+});
+
+arr.map((item, index) => {
+    console.log(index+"번 값", item*2);
+});
+
