d52355434f847c477f1c5a0341dbcdc32fd1126f
sbkim1126 <subink1126@gmail.com>
2023-06-30 00:36:23 +0900

    practice

--- slice_splice.js
+++ slice_splice.js
@@ -1,9 +1,9 @@
-// nums = Array(20).fill().map((_, i) => i);
+nums = Array(20).fill().map((_, i) => i);
 // console.log(nums.splice(5,3));
 // console.log(nums);
 
-// console.log(nums.splice(5, 0, -5, -6, -7));
-// console.log(nums);
+console.log(nums.splice(5, 0, -5, -6, -7));
+console.log(nums);
 
 // console.log(nums.splice(5,2));
 // console.log(nums);
@@ -15,9 +15,10 @@
 // console.log(nums);
 
 num = Array(20).fill().map((_, i) => i);
-console.log(num);
+//console.log(num);
 
-console.log(num.slice(5,8));
-console.log(num);
-console.log(num.splice(5,3));
-console.log(num);
+//console.log(num.slice(1));
+// console.log(num.slice(5,8));
+// console.log(num);
+// console.log(num.splice(5,3));
+// console.log(num);
